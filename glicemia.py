a9af5a1ea60f78f70d611c8df27bb249ca5bdd86
ewerlonosbadboys-eng <ewerlon.osbadboys@gmail.com>
2026-02-22 00:16:27 -0300

    Update glicemia.py

--- glicemia.py
+++ glicemia.py
@@ -7,12 +7,12 @@ import pytz
 
 # 1. Configuracoes Iniciais
 fuso_br = pytz.timezone('America/Sao_Paulo')
-st.set_page_config(page_title="Saude Kids v30", layout="wide")
+st.set_page_config(page_title="Saude Kids v31", layout="wide")
 
-ARQ_G = "dados_glicemia_v30.csv"
-ARQ_N = "dados_nutricao_v30.csv"
+ARQ_G = "dados_glicemia_v31.csv"
+ARQ_N = "dados_nutricao_v31.csv"
 
-# Banco de Alimentos: [Carbo, Prot, Gord, Calorias]
+# Banco de Alimentos: [Carbo, Prot, Gord, Kcal]
 ALIMENTOS = {
     "Pao Frances": [28, 4, 1, 135], 
     "Leite (200ml)": [10, 6, 6, 120], 
@@ -27,9 +27,7 @@ ALIMENTOS = {
 }
 
 def carregar(arq):
-    if os.path.exists(arq):
-        return pd.read_csv(arq)
-    return pd.DataFrame()
+    return pd.read_csv(arq) if os.path.exists(arq) else pd.DataFrame()
 
 def cor_glicemia(v):
     if v == "-" or pd.isna(v): return ""
@@ -38,4 +36,9 @@ def cor_glicemia(v):
         if n < 70: return 'background-color: #FFFF00; color: black'
         if n > 180: return 'background-color: #FF0000; color: white'
         if n > 140: return 'background-color: #FFFF00; color: black'
-        return 'background
+        return 'background-color: #00FF00; color: black'
+    except: return ""
+
+st.title("Monitoramento Saude Kids v31")
+
+aba1, aba2 = st
