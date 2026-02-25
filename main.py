9745cda7779fb1fbd9db1380c996ae91034ac07b
ewerlonosbadboys-eng <ewerlon.osbadboys@gmail.com>
2026-02-24 22:30:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if 'historico' not in st.session_state: st.session_state['historico'] = {}
 aba1, aba2, aba3 = st.tabs(["👥 1. Cadastro", "📅 2. Gerar Escala", "📥 3. Baixar Excel"])
 
 with aba1:
-    st.subheader("Cadastro de Funcionário")
+    st.subheader("Cadastro")
     nome = st.text_input("Nome")
     h_entrada = st.time_input("Entrada", value=datetime.strptime("06:00", "%H:%M").time())
     h_saida = (datetime.combine(datetime.today(), h_entrada) + timedelta(hours=9, minutes=58)).time()
@@ -40,11 +40,11 @@ with aba1:
                 "Nome": nome, "Entrada": h_entrada.strftime('%H:%M'),
                 "Saida": h_saida.strftime('%H:%M'), "Rod_Sab": f_sab, "Casada": f_cas
             })
-            st.success("Cadastrado!")
+            st.success("Salvo!")
 
 with aba2:
     if st.session_state['db_users']:
-        func_sel = st.selectbox("Selecione o Funcionário", [u.get('Nome') for u in st.session_state['db_users']])
+        func_sel = st.selectbox("Funcionário", [u.get('Nome') for u in st.session_state['db_users']])
         if st.button("✨ GERAR ESCALA"):
             datas = pd.date_range(start='2026-03-01', periods=31)
             d_pt = {'Monday':'seg','Tuesday':'ter','Wednesday':'qua','Thursday':'qui','Friday':'sex','Saturday':'sáb','Sunday':'dom'}
@@ -52,61 +52,55 @@ with aba2:
             
             df = pd.DataFrame({'Data': datas, 'Dia': [d_pt[d.day_name()] for d in datas], 'Status': 'Trabalho'})
             
-            # 1. Definir Domingos (Rodízio 1x1)
+            # 1. Definir Domingos (1x1)
             dom_idx = df[df['Dia'] == 'dom'].index.tolist()
             for i, d_idx in enumerate(dom_idx):
                 if i % 2 == 1: # Domingo de Folga
                     df.loc[d_idx, 'Status'] = 'Folga'
+                    # SÓ DÁ FOLGA NA SEGUNDA SE A CAIXINHA ESTIVER MARCADA
                     if user.get("Casada") and (d_idx + 1) < 31:
                         df.loc[d_idx + 1, 'Status'] = 'Folga'
 
-            # 2. Distribuir Folgas Semanais (Segunda a Sábado)
-            # Analisamos cada bloco de Segunda a Domingo
-            for sem_start in range(1, 31, 7): # Começa na primeira segunda do mês
+            # 2. Distribuir demais folgas
+            for sem_start in range(1, 31, 7):
                 bloco_fim = min(sem_start + 7, 31)
-                dias_semana = df.iloc[sem_start:bloco_fim]
+                folgas_no_dom = len(df.iloc[sem_start:bloco_fim][(df['Dia'] == 'dom') & (df['Status'] == 'Folga')])
+                meta_semana = 2 if folgas_no_dom == 0 else 1
                 
-                # Conta quantas folgas já existem no domingo desse bloco
-                folgas_no_domingo = len(dias_semana[(dias_semana['Dia'] == 'dom') & (dias_semana['Status'] == 'Folga')])
+                # Conta folgas na semana (excluindo o domingo)
+                folgas_atuais = len(df.iloc[sem_start:bloco_fim][(df['Status'] == 'Folga') & (df['Dia'] != 'dom')])
                 
-                # Se folgou no domingo, precisa de +1 folga na semana. Se trabalhou no domingo, precisa de +2 folgas.
-                meta_folgas_semana = 2 if folgas_no_domingo == 0 else 1
-                
-                # Se já tem a folga da segunda (casada), desconta da meta
-                folgas_atuais = len(dias_semana[(dias_semana['Status'] == 'Folga') & (dias_semana['Dia'] != 'dom')])
-                
-                while folgas_atuais < meta_folgas_semana:
-                    # Dias possíveis: Segunda a Sexta (Sábado só se marcado rodízio)
-                    pode = dias_semana[(dias_semana['Status'] == 'Trabalho')].index.tolist()
-                    pode = [p for p in pode if df.loc[p, 'Dia'] not in ['dom']]
+                tentativas = 0
+                while folgas_atuais < meta_semana and tentativas < 50:
+                    tentativas += 1
+                    pode = df.iloc[sem_start:bloco_fim][df['Status'] == 'Trabalho'].index.tolist()
+                    pode = [p for p in pode if df.loc[p, 'Dia'] != 'dom']
+                    
                     if not user.get("Rod_Sab"):
                         pode = [p for p in pode if df.loc[p, 'Dia'] != 'sáb']
                     
-                    # Regra crucial: Se NÃO é casada, não pode folgar na segunda após domingo de folga
+                    # REGRA DE OURO: Se não é casada, segunda-feira é PROIBIDA após folga no domingo
                     if not user.get("Casada"):
-                        pode = [p for p in pode if not (df.loc[p, 'Dia'] == 'seg' and df.loc[p-1, 'Status'] == 'Folga' if p > 0 else False)]
+                        pode = [p for p in pode if not (df.loc[p, 'Dia'] == 'seg' and (p > 0 and df.loc[p-1, 'Status'] == 'Folga'))]
 
                     if not pode: break
                     escolha = random.choice(pode)
                     
-                    # Trava anti-dupla na semana
-                    vizinhos_folga = False
-                    if escolha > 0 and df.loc[escolha-1, 'Status'] == 'Folga' and df.loc[escolha-1, 'Dia'] != 'dom': vizinhos_folga = True
-                    if escolha < 30 and df.loc[escolha+1, 'Status'] == 'Folga' and df.loc[escolha+1, 'Dia'] != 'dom': vizinhos_folga = True
+                    # Trava anti-dupla (vizinhos)
+                    viz_f = False
+                    if escolha > 0 and df.loc[escolha-1, 'Status'] == 'Folga': viz_f = True
+                    if escolha < 30 and df.loc[escolha+1, 'Status'] == 'Folga': viz_f = True
                     
-                    if not vizinhos_folga:
+                    if not viz_f:
                         df.loc[escolha, 'Status'] = 'Folga'
                         folgas_atuais += 1
-                    else:
-                        pode.remove(escolha)
-                        if not pode: break
 
             st.session_state['historico'][func_sel] = df
             st.table(df)
 
 with aba3:
     if st.session_state['historico']:
-        f_nome = st.selectbox("Escala:", list(st.session_state['historico'].keys()))
+        f_nome = st.selectbox("Baixar:", list(st.session_state['historico'].keys()))
         df_h = st.session_state['historico'][f_nome]
         u_info = next((i for i in st.session_state['db_users'] if i.get("Nome") == f_nome), None)
         
@@ -128,7 +122,6 @@ with aba3:
                 is_f = (row['Status'] == 'Folga')
                 ent = u_info.get("Entrada", "06:00") if u_info else "06:00"
                 sai = u_info.get("Saida", "15:58") if u_info else "15:58"
-                
                 c_t = ws.cell(3, col, "Folga" if is_f else ent)
                 c_b = ws.cell(4, col, "" if is_f else sai)
                 c_t.alignment = c_b.alignment = center
