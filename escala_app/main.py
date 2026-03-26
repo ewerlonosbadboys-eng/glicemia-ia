81670e908746ac470002c1f5f92d94df36308421
ewerlonosbadboys-eng <ewerlon.osbadboys@gmail.com>
2026-03-26 10:09:01 -0300

    Update main.py

--- escala_app/main.py
+++ escala_app/main.py
@@ -19914,7 +19914,7 @@ def _fast_restore_bundled_latest_before_start() -> None:
 
 # =========================================================
 # MAIN
-# =========================================================
+# ========================================================= 
 _fast_restore_bundled_latest_before_start()
 validar_contrato_sistema()
 
